d69013ebc3718d09ae7640fcbea648c418cdadb5
selinozluk <selinozluk36@gmail.com>
2025-07-09 11:17:26 +0300

    Add project files.

---
+++ Constructor/Bebek.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    public class Bebek
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Birthdate { get; set; }
+
+
+        // 1. Parametre Almayan Default Constructor 
+        public Bebek() 
+        {
+            Console.WriteLine("Ingaaaa");
+           Birthdate = DateTime.Now;
+        }
+
+        // 2. Parametre alan Constructor
+
+        public Bebek (string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+            Birthdate = DateTime.Now;
+        }
+
+    }
+}
---
+++ Constructor/Program.cs
@@ -0,0 +1,23 @@
+﻿using Constructor;
+
+
+// 1. Default constructor
+
+Bebek baby1 = new Bebek ();
+baby1.Name = "Selin";
+baby1.Surname = "Özlük";
+
+Console.WriteLine($"Bebeğin İsmi: {baby1.Name}");
+Console.WriteLine($"Bebeğin Soyismi: {baby1.Surname}");
+Console.WriteLine($"Bebeğin Doğum Tarihi: {baby1.Birthdate}");
+
+Console.WriteLine("-------------------------------------");
+
+// 2. Parametreli constructor
+
+Bebek baby2 = new Bebek ("Billie", "Eilish");
+
+Console.WriteLine($"Bebeğin İsmi: {baby2.Name}");
+Console.WriteLine($"Bebeğin Soyismi: {baby2.Surname}");
+Console.WriteLine($"Bebeğin Doğum Tarihi: {baby2.Birthdate}");
+
